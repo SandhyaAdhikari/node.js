76ecd4e66cc2f6a368c381dad5e63c89a460dabd
Sandhya <157025340+SandhyaAdhikari@users.noreply.github.com>
2024-05-17 18:59:40 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-// import os from "os";
-// console.log(os.release());
-// console.log(os.version());
-// console.log(os.cpus());
-// console.log(os.freemem());
+import express from "express";
+//create app
+const app = express();
+// used to make app understand json
+// json= java script object notation
 
-import fs from "fs";
-fs.writeFileSync("hello.txt", "hello, welcome to MERN");
-fs.appendFileSync("hello.txt", "i dont know whats going on");
+app.use(express.json());
+
+//?req methods
+//? post (create/add).get(read/retrieve), put (edit/update), delete (remove.delete)
+
+//routes
+// app.get("/say.hello", (req, res) => {
+//   return res.status(200).send("hello this is magic im pissed");
+// });
+// app.post("/student/add", (req, res) => {
+//   return res.status(201).send({ message: "student add api hit..." });
+// });
+
+//add student
+let studentlist = [];
+app.post("/student/add", (req, res) => {
+  const newStudent = req.body;
+  studentlist.push(newStudent);
+  return res.status(200).send({ message: "student is added successfully" });
+});
+app.get("/student/list", (req, res) => {
+  return res.status(200).send({ message: "success", students: studentlist });
+});
+
+//network port
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`app is listening on port ${PORT}`);
+});
